b1f6f69d3abfe3dde1b99864e1e5dbcd9b7c6c34
türkay ürkmez <turkay.urkmez@outlook.com>
2025-03-11 10:34:29 +0300

    Adding Category entity

---
+++ StockTracker/StockTracker.Domain/Aggregates/Category.cs
@@ -0,0 +1,38 @@
+﻿using Ardalis.GuardClauses;
+using StockTracker.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracker.Domain.Aggregates
+{
+    public class Category :  BaseEntity<int>, IAggregateRoot
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        
+
+        private Category()
+        {
+            // required by EF
+        }
+
+        public Category(string name, string description)
+        {
+            Guard.Against.NullOrEmpty(name, nameof(name), "Kategori adı boş olamaz");
+            Guard.Against.NullOrEmpty(description, nameof(description), "Kategori açıklaması boş olamaz");
+            Name = name;
+            Description = description;
+        }
+
+        public void UpdateCategory(string name, string description)
+        {
+            Guard.Against.NullOrEmpty(name, nameof(name), "Kategori adı boş olamaz");
+            Guard.Against.NullOrEmpty(description, nameof(description), "Kategori açıklaması boş olamaz");
+            Name = name;
+            Description = description;
+        }
+    }
+}
--- StockTracker/StockTracker.Domain/Aggregates/Product.cs
+++ StockTracker/StockTracker.Domain/Aggregates/Product.cs
@@ -1,4 +1,6 @@
-﻿using StockTracker.Domain.Common;
+﻿using Ardalis.GuardClauses;
+using StockTracker.Domain.Common;
+using StockTracker.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,75 @@ namespace StockTracker.Domain.Aggregates
     public class Product : BaseEntity<Guid>, IAggregateRoot
     {
         public string Name { get; private set; }
+        public string SKU { get; private set; }
+        public string Description { get; private set; }
+        public Money Price { get; private set; }
+        public bool IsActive { get; private set; }
+        public int StockQuantity { get; private set; }
+        public string? ImageUrl { get; private set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        private Product()
+        {
+            // required by EF
+        }
+
+        public Product(string name, string sku, string description, Money price, bool isActive, int stockQuantity, string? imageUrl, int categoryId)
+        {
+
+            Guard.Against.NullOrEmpty(name, nameof(name), "Ürün adı boş olamaz");
+            Guard.Against.NullOrEmpty(sku, nameof(sku), "Ürün sku boş olamaz");
+            Guard.Against.NegativeOrZero(Price.Amount, nameof(Price), "Ürün fiyatı 0'dan küçük olamaz");
+
+
+            Name = name;
+            SKU = sku;
+            Description = description;
+            Price = price;
+            IsActive = isActive;
+            StockQuantity = stockQuantity;
+            ImageUrl = imageUrl;
+            CategoryId = categoryId;
+        }
+
+        public void UpdateProduct(string name, string sku, string description, Money price, string? imageUrl, int categoryId)
+        {
+            Guard.Against.NullOrEmpty(name, nameof(name), "Ürün adı boş olamaz");
+            Guard.Against.NullOrEmpty(sku, nameof(sku), "Ürün sku boş olamaz");
+            Guard.Against.NegativeOrZero(Price.Amount, nameof(Price), "Ürün fiyatı 0'dan küçük olamaz");
+
+            Name = name;
+            SKU = sku;
+            Description = description;
+            Price = price;
+            CategoryId = categoryId;
+
+            //IsActive = isActive;
+            //StockQuantity = stockQuantity;
+
+            //DİKKAT!
+            //Bazı özelliklerin güncellenmesi için ekstra metotlar yazdık.
+
+            ImageUrl = imageUrl;
+        }
+
+        public void IncreaseStock(int quantity)
+        {
+            Guard.Against.Negative(quantity, nameof(quantity), "Stok miktarı 0'dan küçük olamaz");
+            StockQuantity += quantity;
+        }
+
+        public void DecreaseStock(int quantity)
+        {
+            Guard.Against.Negative(quantity, nameof(quantity), "Stok miktarı 0'dan küçük olamaz");
+            StockQuantity -= quantity;
+        }
+
+        public void Activate()=> IsActive = true;
+        public void Deactivate() => IsActive = false;
+
 
 
 
